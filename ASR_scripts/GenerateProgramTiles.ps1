42c21b9267e1bf3bf7544b6e877c2d800f925508
j0shbregman <96197424+j0shbregman@users.noreply.github.com>
2023-02-05 15:29:04 -0500

    Updated for v6 beta

--- ASR_scripts/GenerateProgramTiles.ps1
+++ ASR_scripts/GenerateProgramTiles.ps1
@@ -24,7 +24,7 @@
 
 
 
-
+#These are the paths where the links are going to be 
 $link_paths = @("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\", "$env:PROGRAMDATA\Microsoft\Windows\Start Menu\Programs\")
 
 $start_apps = (Get-StartApps)
@@ -89,6 +89,9 @@ $programs = @{
 }
 
 
+
+
+
 function GetLinkData($path, $link) {
     
     $filepath = $path+$link
@@ -103,6 +106,19 @@ function GetLinkData($path, $link) {
     $app_id = $ShellFile.ExtendedProperty("System.AppUserModel.ID")
     $toast_activator_CLSID = $ShellFile.ExtendedProperty("System.AppUserModel.ToastActivatorCLSID")
     $package_app_id = $ShellFile.ExtendedProperty("System.AppUserModel.PinMigration.PackagedAppId")
+
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($filepath)
+    
+
+    $target_path = $Shortcut.TargetPath
+
+    if ($null -ne $target_path -and $target_path -ne "") {
+
+        $target_paths = $target_path -split '\\'
+        $last_target_path = $target_paths[$target_paths.Count-1]
+
+    }
 
     if ($null -ne $app_id -and $start_apps_map.Contains(($app_id))) {
         $app_name = $start_apps_map[$app_id]
@@ -120,7 +136,7 @@ function GetLinkData($path, $link) {
     if ($null -ne $app_id -or $null -ne $toast_activator_CLSID -or $null -ne $package_app_id) {
 
         if ($app_name -ne $last_link_name) {
-            #Write-Host "Weird!!!!!! $app_name $last_link_name"
+            Write-Host "Warning: The name of the app ($app_name) and the link ($last_link_name.lnk) don't match." -ForegroundColor Yellow
         }
 
 
@@ -130,32 +146,100 @@ function GetLinkData($path, $link) {
             $sub_menu = ""
         }
         
+        #Check if the name of the app or the .exe is in the $programs list
         if ($programs.Contains($last_link_name)) {
 
             $comment = ""
+            #remove the item from programs
+
+            $program = $programs[$last_link_name]
+
+            $program_data = "`t""$last_link_name""        =      ""$program""`n"
+
+            $programs.Remove($last_link_name)
+
+
+        } elseif ($programs.ContainsValue($last_target_path)) {
+            
+            #Find the key for multi-versions
+            $multi_version_key = $programs.Keys.Where({ $programs[$PSItem] -eq $last_target_path; }, [System.Management.Automation.WhereOperatorSelectionMode]::First);
+            $program_data = "`t""$multi_version_key""        =      ""$last_target_path""`n"
+
+            if ($programs.Contains("$multi_version_key")) {
+                $programs.Remove("$multi_version_key")
+            } else {
+                Write-Host "Error:  `$programs doesn't contain $multi_version_key" -ForegroundColor Red
+            }
+            
+
 
         } else {
-        
+
+            Write-Host "Warning: No entry or duplicate entry for $last_link_name or $last_target_path in `$programs." -ForegroundColor Yellow
+            if ($null -ne $target_path -and $target_path -ne "") {
+
+                $program_data="`t#""$last_link_name""      =   ""$last_target_path""`n"  
+
+            }
             $comment = "#"
         }
 
-        Write-Host "`t$comment""$last_link_name"" = @{AppUserModelID=""$app_id"";ToastActivatorCLSID=""$toast_activator_CLSID"";PackagedAppId=""$package_app_id"";SubMenu=""$sub_menu"";AppNameForId=""$app_name""}"
+        if ($null -ne $toast_activator_CLSID ) {
+            #remove the {}
+            $toast_activator_CLSID = $toast_activator_CLSID.trim('{}') 
+            $toast_activator_CLSID = "[System.guid]::New(""$toast_activator_CLSID"")"
+        } else {
+            $toast_activator_CLSID = """"""
+        }
+
+        $tiles_data = "`t$comment""$last_link_name"" = @{AppUserModelID=""$app_id"";ToastActivatorCLSID=$toast_activator_CLSID;PackagedAppId=""$package_app_id""}`n"
             
 
+        return $tiles_data, $program_data
         
     }
     
 
 }
 
-Write-Host "`$app_data=@{"
+
+
+$tile_output = ""
+$program_output = ""
+
+
+$tile_output = "`$programsTiles=@{`n"
+$program_output = "`$programs=@{`n"
+
 foreach ($path in $link_paths) {
 
     
     $links = (Get-ChildItem -Path $path -Recurse -Name -Include "*.lnk")
     foreach ($link in $links) {
-            GetLinkData $path $link
+        $tile_data,$program_data=GetLinkData $path $link
+        $tile_output+= $tile_data
+        $program_output+=$program_data
     }
 
 }
-Write-Host "}"
+$tile_output += "}"
+
+
+$programs.GetEnumerator() | ForEach-Object {
+
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "Warning: No link found for $key.  Please re-run this script on a machine that has this application installed correctly." -ForegroundColor Yellow
+    $program_output += "`t""$key""        =      ""$value""`n"
+}
+$program_output +="}"
+
+
+Write-Host "Entries that are commented out were found on the machine, but not in the `$programs variable on line 42 of this script." -ForegroundColor Green
+Write-Host "Use the following data in the AddShortcutsv6.ps1 for `$programsTiles" -ForegroundColor Green
+
+Write-Host "$tile_output"
+
+Write-Host "Use the following data in the AddShortcutsv6.ps1 for `$programs" -ForegroundColor Green
+
+Write-Host "$program_output"
---
+++ ASR_scripts/TestProgramTiles.ps1
@@ -0,0 +1,58 @@
+$programsTiles=@{
+    #"Administrative Tools" = @{AppUserModelID="Microsoft.Windows.AdministrativeTools";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Amazon Music" = @{AppUserModelID="Amazon.Music";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Bot Framework Emulator (V4)" = @{AppUserModelID="F3C061A6-FE81-4548-82ED-C1171D9856BB";ToastActivatorCLSID="";PackagedAppId=""}
+    #"File Explorer" = @{AppUserModelID="Microsoft.Windows.Explorer";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Microsoft Teams (work or school)" = @{AppUserModelID="com.squirrel.Teams.Teams";ToastActivatorCLSID=[System.guid]::New("D1B22D3D-8585-53A6-ACB3-0E803C7E8D2A");PackagedAppId=""}
+    "OneDrive" = @{AppUserModelID="Microsoft.SkyDrive.Desktop";ToastActivatorCLSID=[System.guid]::New("6BB93B4E-44D8-40E2-BD97-42DBCF18A40F");PackagedAppId=""}
+    "Outlook" = @{AppUserModelID="Microsoft.Office.OUTLOOK.EXE.15";ToastActivatorCLSID=[System.guid]::New("0006F0C6-0000-0000-C000-000000000046");PackagedAppId=""}
+    #"Chromebook Recovery Utility" = @{AppUserModelID="Chrome._crx_jndclhbbiimdgai.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Cracking Sands Racing" = @{AppUserModelID="Chrome._crx_pnafpdnmojicndb.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Cut the Rope" = @{AppUserModelID="Chrome._crx_jfbadkgaackjmdo.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Dialpad" = @{AppUserModelID="Chrome._crx_hfbikcpkbicmndn.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Google Hangouts" = @{AppUserModelID="Chrome._crx_knipogolnpehhpl.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Murder Files" = @{AppUserModelID="Chrome._crx_ijfeciahggomhaf.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"RAD Soldiers" = @{AppUserModelID="Chrome._crx_dkiaheagmoembpe.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Solve a Cipher!" = @{AppUserModelID="Chrome._crx_lmgkpanfnbpogcd.UserData.Profile2";ToastActivatorCLSID="";PackagedAppId=""}
+    #"GitHub Desktop" = @{AppUserModelID="com.squirrel.GitHubDesktop.GitHubDesktop";ToastActivatorCLSID=[System.guid]::New("27D44D0C-A542-5B90-BCDB-AC3126048BA2");PackagedAppId=""}
+    #"Outlook" = @{AppUserModelID="com.squirrel.Outlook.olk";ToastActivatorCLSID=[System.guid]::New("CF57AB46-9D90-5E9D-B5EC-B13A74E5B68F");PackagedAppId=""}
+    #"Control Panel" = @{AppUserModelID="Microsoft.Windows.ControlPanel";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Run" = @{AppUserModelID="Microsoft.Windows.Shell.RunDialog";ToastActivatorCLSID="";PackagedAppId=""}
+    "Visual Studio Code" = @{AppUserModelID="Microsoft.VisualStudioCode";ToastActivatorCLSID="";PackagedAppId=""}
+    "Zoom" = @{AppUserModelID="zoom.us.Zoom Video Meetings";ToastActivatorCLSID="";PackagedAppId=""}    
+    "Access" = @{AppUserModelID="Microsoft.Office.MSACCESS.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    "Adobe Creative Cloud" = @{AppUserModelID="Adobe.CreativeCloud";ToastActivatorCLSID=[System.guid]::New("2F81B25E-7507-4844-BFF2-77D2CC24CED4");PackagedAppId=""}
+    #"Blend for Visual Studio 2022" = @{AppUserModelID="Blend.f4a590ce";ToastActivatorCLSID="";PackagedAppId=""}
+    "Excel" = @{AppUserModelID="Microsoft.Office.EXCEL.EXE.15";ToastActivatorCLSID="";PackagedAppId=""} 
+    "Google Chrome" = @{AppUserModelID="Chrome";ToastActivatorCLSID=[System.guid]::New("A2C6CB58-C076-425C-ACB7-6D19D64428CD");PackagedAppId=""}
+    "Microsoft Edge" = @{AppUserModelID="MSEdge";ToastActivatorCLSID=[System.guid]::New("628ACE20-B77A-456F-A88D-547DB6CEEDD5");PackagedAppId="Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge"}
+    "OneNote" = @{AppUserModelID="Microsoft.Office.ONENOTE.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Outlook" = @{AppUserModelID="Microsoft.Office.OUTLOOK.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    "PowerPoint" = @{AppUserModelID="Microsoft.Office.POWERPNT.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    "Publisher" = @{AppUserModelID="Microsoft.Office.MSPUB.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    "Skype for Business" = @{AppUserModelID="Microsoft.Office.lync.exe.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Visual Studio 2022" = @{AppUserModelID="VisualStudio.f4a590ce";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Visual Studio Installer" = @{AppUserModelID="Microsoft.VisualStudio.Installer";ToastActivatorCLSID="";PackagedAppId=""}
+    "Word" = @{AppUserModelID="Microsoft.Office.WINWORD.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Remote Desktop Connection" = @{AppUserModelID="Microsoft.Windows.RemoteDesktop";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Windows Media Player Legacy" = @{AppUserModelID="Microsoft.Windows.MediaPlayer32";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Software Center" = @{AppUserModelID="Microsoft.SoftwareCenter.DesktopToasts";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Microsoft Intune Management Extension" = @{AppUserModelID="Microsoft.Management.Clients.IntuneManagementExtension";ToastActivatorCLSID=[System.guid]::New("F0B4FA28-6199-4673-85F2-C9151C150D0A");PackagedAppId=""}
+    #"Database Compare" = @{AppUserModelID="Microsoft.Office.DATABASECOMPARE.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Office Language Preferences" = @{AppUserModelID="Microsoft.Office.SETLANG.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Skype for Business Recording Manager" = @{AppUserModelID="Microsoft.Office.OcPubMgr.exe.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Spreadsheet Compare" = @{AppUserModelID="Microsoft.Office.SPREADSHEETCOMPARE.EXE.15";ToastActivatorCLSID="";PackagedAppId=""}
+    #"Telemetry Log for Office" = @{AppUserModelID="Microsoft.Office.msoev.exe.15";ToastActivatorCLSID="";PackagedAppId=""}
+}
+
+$programsTiles.Count
+
+
+
+$programsTiles.GetEnumerator() | ForEach-Object {
+
+    Write-Host $_.Key
+    Write-Host $_.Value["AppUserModelID"] $_.Value["ToastActivatorCLSID"] $_.Value["PackagedAppId"]
+
+
+}
